82ee7877e9476357163255c6ee34bb8ef41d1286
N Roshan Vali <nroshanvali786@gmail.com>
2026-03-03 20:36:35 +0530

    Update LogoutController.java

--- Backend/src/main/java/com/example/demo/logout/LogoutController.java
+++ Backend/src/main/java/com/example/demo/logout/LogoutController.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", "https://gadgeo-ktfjs5omh-nroshanvali786s-projects.vercel.app", allowCredentials = "true")
 @RequestMapping("/api")
 public class LogoutController {
 
