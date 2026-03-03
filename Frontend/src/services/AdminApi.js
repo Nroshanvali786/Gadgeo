390636a539e13275a45c352659d12f1458c58b76
N Roshan Vali <nroshanvali786@gmail.com>
2026-03-03 20:25:38 +0530

    Update AdminApi.js

--- Frontend/src/services/AdminApi.js
+++ Frontend/src/services/AdminApi.js
@@ -1,6 +1,7 @@
 // src/services/adminApi.js
 
-const BASE_URL = "http://localhost:8080/admin";
+// const BASE_URL = "http://localhost:8080/admin";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const addProduct = async (productData) => {
   const response = await fetch(`${BASE_URL}/products/add`, {
