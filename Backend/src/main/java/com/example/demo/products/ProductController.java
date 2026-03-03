c9b4204bc026d32d852c1fa82c335f54f5837519
N Roshan Vali <nroshanvali786@gmail.com>
2026-03-03 21:12:42 +0530

    Update ProductController.java

--- Backend/src/main/java/com/example/demo/products/ProductController.java
+++ Backend/src/main/java/com/example/demo/products/ProductController.java
@@ -18,9 +18,9 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin(
-    origins = {
+    originPatterns = {
         "http://localhost:5173",
-        "https://gadgeo-ktfjs5omh-nroshanvali786s-projects.vercel.app"
+        "https://*.vercel.app"
     },
     allowCredentials = "true"
 )
