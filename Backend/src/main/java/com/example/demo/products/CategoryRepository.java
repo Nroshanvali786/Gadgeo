067c936e038b723321f2b37857034a4eee235e7c
N Roshan Vali <nroshanvali786@gmail.com>
2026-03-06 22:07:35 +0530

    Update CategoryRepository.java

--- Backend/src/main/java/com/example/demo/products/CategoryRepository.java
+++ Backend/src/main/java/com/example/demo/products/CategoryRepository.java
@@ -10,7 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    Optional<Category> findByCategoryName(String categoryName);
+    // Optional<Category> findByCategoryName(String categoryName);
+
+    Optional<Category> findByCategoryNameIgnoreCase(String categoryName);
     
 
 }
